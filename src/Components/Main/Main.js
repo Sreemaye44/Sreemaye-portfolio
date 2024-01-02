8f01b8e7d61e02a3a086c5e3a7a53972f9b33a71
sreemaye <sreemaye44@gmail.com>
2024-01-02 12:13:42 +0600

    experience folder created

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import Banner from '../Banner/Banner';
-import Contact from '../Contact/Contact';
-import ContactOption from '../Contact/ContactOption';
-import Navbar from '../Navbar/Navbar';
-import Project from '../Projects/Project';
-import SkillBar from '../SkillBar/SkillBar';
+import React from "react";
+import Banner from "../Banner/Banner";
+import Contact from "../Contact/Contact";
+import ContactOption from "../Contact/ContactOption";
+import Navbar from "../Navbar/Navbar";
+import Project from "../Projects/Project";
+import SkillBar from "../SkillBar/SkillBar";
 
 const Main = () => {
-    return (
-        <div>
-            <div>
-            <Navbar></Navbar>
-            </div>
-            <div>
-                <Banner></Banner>
-                <SkillBar></SkillBar>
-            <Project></Project>
-            <ContactOption></ContactOption>
-                </div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <Navbar></Navbar>
+      </div>
+      <div>
+        <Banner></Banner>
+        <SkillBar></SkillBar>
+        <Project></Project>
+        <ContactOption></ContactOption>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
--- src/Components/SkillBar/SkillBar.js
+++ src/Components/SkillBar/SkillBar.js
@@ -1,93 +1,93 @@
-import React from 'react';
-import './SkillBar.css'
+import React from "react";
+import "./SkillBar.css";
 
 const SkillBar = () => {
-    return (
-        <body className='text-white' id='skill'>
-    <div className="skills_section">
+  return (
+    <body className="text-white" id="skill">
+      <div className="skills_section">
         <div className="skills_head">
-            <h2>My <span>Skills</span></h2>
-            <p>Here is my skills to represent my Expertise</p>
+          <h2>
+            My <span>Skills</span>
+          </h2>
         </div>
 
         <div className="skills_main">
-            <div className="skill_bar">
-                <div className="info">
-                    <p>HTML</p>
-                    <p>90%</p>
-                </div>
-                <div className="bar">
-                    <span className="html"></span>
-                </div>
+          <div className="skill_bar">
+            <div className="info">
+              <p>Java Script / TypeScript</p>
+              <p>85%</p>
             </div>
-            <div className="skill_bar">
-                <div className="info">
-                    <p>CSS</p>
-                    <p>85%</p>
-                </div>
-                <div className="bar">
-                    <span className="css"></span>
-                </div>
+            <div className="bar">
+              <span className="html"></span>
             </div>
-            <div className="skill_bar">
-                <div className="info">
-                    <p>SASS</p>
-                    <p>80%</p>
-                </div>
-                <div className="bar">
-                    <span className="sass"></span>
-                </div>
+          </div>
+          <div className="skill_bar">
+            <div className="info">
+              <p>CSS/Tailwind/MUI</p>
+              <p>85%</p>
             </div>
-            <div className="skill_bar">
-                <div className="info">
-                    <p>Java Script</p>
-                    <p>80%</p>
-                </div>
-                <div className="bar">
-                    <span className="js"></span>
-                </div>
+            <div className="bar">
+              <span className="css"></span>
             </div>
-            <div className="skill_bar">
-                <div className="info">
-                    <p>React js</p>
-                    <p>75%</p>
-                </div>
-                <div className="bar">
-                    <span className="react"></span>
-                </div>
+          </div>
+          <div className="skill_bar">
+            <div className="info">
+              <p>Angular</p>
+              <p>70%</p>
             </div>
-            <div className="skill_bar">
-                <div className="info">
-                    <p>Node js</p>
-                    <p>70%</p>
-                </div>
-                <div className="bar">
-                    <span className="node"></span>
-                </div>
+            <div className="bar">
+              <span className="js"></span>
             </div>
-            <div className="skill_bar">
-                <div className="info">
-                    <p>Express js</p>
-                    <p>65%</p>
-                </div>
-                <div className="bar">
-                    <span className="express"></span>
-                </div>
+          </div>
+          <div className="skill_bar">
+            <div className="info">
+              <p>React js/Next JS</p>
+              <p>80%</p>
             </div>
-            <div className="skill_bar">
-                <div className="info">
-                    <p>Mongo DB</p>
-                    <p>60%</p>
-                </div>
-                <div className="bar">
-                    <span className="mongo"></span>
-                </div>
+            <div className="bar">
+              <span className="react"></span>
             </div>
+          </div>
+          <div className="skill_bar">
+            <div className="info">
+              <p>Node js (Express)</p>
+              <p>70%</p>
+            </div>
+            <div className="bar">
+              <span className="node"></span>
+            </div>
+          </div>
+          <div className="skill_bar">
+            <div className="info">
+              <p>Mongo DB/ Moongoose</p>
+              <p>60%</p>
+            </div>
+            <div className="bar">
+              <span className="mongo"></span>
+            </div>
+          </div>
+          <div className="skill_bar">
+            <div className="info">
+              <p>MySql</p>
+              <p>60%</p>
+            </div>
+            <div className="bar">
+              <span className="mongo"></span>
+            </div>
+          </div>
+          <div className="skill_bar">
+            <div className="info">
+              <p>Git/Github/GitLab</p>
+              <p>80%</p>
+            </div>
+            <div className="bar">
+              <span className="mongo"></span>
+            </div>
+          </div>
         </div>
-    </div>
-</body>
-
-    );
+      </div>
+    </body>
+  );
 };
 
 export default SkillBar;
